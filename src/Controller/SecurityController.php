2bb189dbc529176875dc5f24e2fa44febeff232a
AurelienGuern <aguern96@gmail.com>
2024-03-20 17:01:05 +0100

    registration OK

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,42 +9,45 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
-            $entityManager->persist($user);
-            try {
-                $entityManager->flush();
-            } catch (\Throwable $th) {
-                if ($th->getCode() === 1062) {
-                    $this->addFlash('warning', 'Cette adresse mail existe déjà');
-                } else {
-                    throw $th;
-                }
-                
-            }
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
-            'form' => $form,
+            'last_username' => $lastUsername,
             'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/register', name: 'app_register', methods:['GET', 'POST'])]
+    public function registration(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User;
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre inscription a été réussie.');
+
+            return $this->redirectToRoute('front_main_home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,13 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
@@ -19,35 +19,50 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('email', EmailType::class, [
-            'label' => 'Courriel',
-            'empty_data'    => '',
-        ])
-        ->add('firstname', TextType::class, [
-            'label' => 'firstname',
-            'empty_data'    => '',
-        ])
-        ->add('lastname', TextType::class, [
-            'label' => 'lastname',
-            'empty_data'    => '',
-        ])
-            ->add('password', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'attr' => ['autocomplete' => 'new-password'],
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => "2",
+                    'maxlength' => '25'
+                ],
+                'label' => 'Email',
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                    new NotBlank(['message' => 'Veuillez entrer votre adresse email']),
+                    new Email(['message' => 'L\'adresse email "{{ value }}" n\'est pas valide.']),
                 ],
             ])
-        ;
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Prénom',
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre prénom']),
+                    new Length(['min' => 2, 'max' => 255]),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Nom',
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre nom']),
+                    new Length(['min' => 2, 'max' => 255]),
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'form-control'],
+                'label' => 'Mot de passe',
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez entrer votre mot de passe']),
+                    new Length(['min' => 6, 'max' => 4096, 'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères']),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary mt-4'],
+                'label' => 'S\'inscrire',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
